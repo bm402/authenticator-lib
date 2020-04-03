227e4f9d991c6b4c8ee82203b0b1a62f2d9e1eae
bncrypted <bmdfgh.b@gmail.com>
2020-04-03 13:08:55 +0100

    spring skeleton

---
+++ src/main/java/com/bncrypted/authenticator/AuthenticatorLibApplication.java
@@ -0,0 +1,13 @@
+package com.bncrypted.authenticator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticatorLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticatorLibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bncrypted/authenticator/AuthenticatorLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bncrypted.authenticator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticatorLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
