5e70b4eaf2093f43a2f451e9063ceffec7e79005
bncrypted <bmdfgh.b@gmail.com>
2020-04-08 18:54:14 +0100

    fix user token exception handler

--- src/main/java/com/bncrypted/authenticator/parser/core/user/exception/UserTokenInvalidException.java
+++ src/main/java/com/bncrypted/authenticator/parser/core/user/exception/UserTokenInvalidException.java
@@ -1,6 +1,6 @@
 package com.bncrypted.authenticator.parser.core.user.exception;
 
-public class UserTokenInvalidException extends RuntimeException {
+public class UserTokenInvalidException extends UserTokenException {
 
     public UserTokenInvalidException() {
         super("User token is not valid");
